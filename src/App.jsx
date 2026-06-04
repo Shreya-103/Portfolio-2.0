acf39e97a21c5b5d35347e8498d372c1d568bcde
Shreya-103 <shreyaalpha1@gmail.com>
2026-06-04 23:34:08 +0530

    format

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import Navbar from "./components/Navbar/navbar";
 import AboutMe from "./components/AboutMe/aboutme";
 import Projects from "./components/Project/projects";
 import TechSkills from "./components/Skills/techskills";
-// import contactForm from "./components/Contact/contactform"; 
 import ContactSection from "./components/Contact/contactform";
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <>
-    <Navbar/>
-    <AboutMe/>
-    <TechSkills/>
-    <Projects/>
-    <ContactSection/>
+      <Navbar />
+      <AboutMe />
+      <TechSkills />
+      <Projects />
+      <ContactSection />
     </>
   )
 }
