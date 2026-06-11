6eeaf4f6321b5f10fa9fadf6d3a7e37aeb2c9934
Shreya-103 <shreyaalpha1@gmail.com>
2026-06-11 09:11:45 +0530

    project details update

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -8,19 +8,8 @@ const project = [
     demo: "https://shreya-103.github.io/Web-Using-Bootstrap",
     github: "https://github.com/Shreya-103/Web-Using-Bootstrap",
   },
-
   {
     id: 2,
-    title: "Notes App",
-    image: "./Screenshot (98).jpg",
-    description:
-      "A responsive Notes App built using React that allows users to create, search, and manage notes efficiently with a clean and interactive UI. Integrated localStorage for persistent data saving, dark mode support, real-time updates, and responsive design for seamless usage across devices.",
-    demo: "https://shreya-103.github.io/Notes-App",
-    github: "https://github.com/Shreya-103/Notes-App",
-  },
-
-  {
-    id: 3,
     title: "CURRENCY CONVERTER",
     image: "./Screenshot (93).png",
     description:
@@ -29,22 +18,31 @@ const project = [
     github: "https://github.com/Shreya-103/currency-converter",
   },
   {
-    id: 4,
-    title: "Employee Entry System",
-    image: "./bel-project.png",
+    id: 3,
+    title: "Notes App",
+    image: "./Screenshot (98).jpg",
     description:
-      "Full-stack A role-based employee entry management system; Features separate employee and admin login authentication with a responsive UI.; Made during internship at BEL , Employee credentials for login: EmployeeID: UPT223 | Password: 12345, Admin credentials:- username: admin | password: 1234",
-    demo: "https://employee-entry-login.netlify.app",
-    github: "https://github.com/Shreya-103/employee-entry-login",
+      "A responsive Notes App built using React that allows users to create, search, and manage notes efficiently with a clean and interactive UI. Integrated localStorage for persistent data saving, dark mode support, real-time updates, and responsive design for seamless usage across devices.",
+    demo: "https://shreya-103.github.io/Notes-App",
+    github: "https://github.com/Shreya-103/Notes-App",
   },
   {
-    id: 5,
+    id: 4,
     title: "Friendship Analyzer fun game",
     image: "./Friendship-Analyzer.png",
     description:
       "Friendship compatibility score generation, Friendship levels based on score, Loading animations, Dark mode support, Responsive design for mobile and desktop",
     demo: "https://shreya-103.github.io/Calculate-Bond/",
     github: "https://github.com/Shreya-103/Calculate-Bond",
+  },
+  {
+    id: 5,
+    title: "Employee Entry System",
+    image: "./bel-project.png",
+    description:
+      "Full-stack A role-based employee entry management system; Features separate employee and admin login authentication with a responsive UI.; Made during internship at BEL , Employee credentials for login: EmployeeID: UPT223 | Password: 12345, Admin credentials:- username: admin | password: 1234",
+    demo: "https://employee-entry-login.netlify.app",
+    github: "https://github.com/Shreya-103/employee-entry-login",
   },
 ];
 
